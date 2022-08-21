3f7d00999c442527cef1d85dbe0f6c843d31bcac
sowmiyaramanathan <76054140+sowmiyaramanathan@users.noreply.github.com>
2022-08-21 19:55:57 +0530

    Added_Output

--- Week_4/Python/Expense_for_the_wedding.py
+++ Week_4/Python/Expense_for_the_wedding.py
@@ -18,10 +18,12 @@ print("Total Expense for the wedding : Rs.", total_expense_for_the_wedding)     
 #Enter the lunch cost per person : 30
 #Enter the number of persons : 124
 #Total Expense for the wedding : Rs. 8556.0
+
 #for lunch cost Rs.0
 #Enter the lunch cost per person : 0
 #Enter the number of persons : 100
 #Total Expense for the wedding : Rs. 0.0
+
 #for lunch cost Rs.50
 #Enter the lunch cost per person : 50
 #Enter the number of persons : 500
