46476934407c3500769c14f965d939a625b85986
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-09-01 22:50:48 +0530

    Added_comments

--- Week_8/Odd_Even_index.py
+++ Week_8/Odd_Even_index.py
@@ -1,13 +1,13 @@
-word = input("Enter a word : ")
+word = input("Enter a word : ")     #reading input
 
 print("Letter in odd index : ")
-for letter in range(1, len(word), 2):
-    print(word[letter], end = "")
+for letter in range(1, len(word), 2):       #iterating through the word by odd index
+    print(word[letter], end = "")           #printing the letter
 
 print("\nLetter in even index : ")
-if(len(word) % 2 == 0):
-    for letter in range(len(word)-2, -1, -2):
-        print(word[letter], end = "")
+if(len(word) % 2 == 0):             #checking whether the length of the word is even to fix the starting index
+    for letter in range(len(word)-2, -1, -2):       #iterating through the word by even index
+        print(word[letter], end = "")               #printing the letter
 else:
-    for letter in range(len(word)-1, -1, -2):
-        print(word[letter], end = "")
+    for letter in range(len(word)-1, -1, -2):       #iterating through the word by even index
+        print(word[letter], end = "")               #printing the letter
