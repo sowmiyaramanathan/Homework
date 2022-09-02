7148393d1acf20d37199fdcec1131dca6ecbdcac
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-09-02 20:59:06 +0530

    Modified_condition_variables

--- Week_8/Odd_Even_index.py
+++ Week_8/Odd_Even_index.py
@@ -1,18 +1,17 @@
 word = input("Enter a word : ")     #reading input
 
-odd_index = []                      #initializing a list to store elements in odd index
-even_index = []                     #initializing a list to store elements in even index
+print("Letter in odd index : ")
+for letter in range(1, len(word), 2):       #iterating through the word by odd index
+    print(word[letter], end = "")           #printing the letter
 
-for letter in range(0, len(word)):          #iterating through the word
-    if(letter%2 == 0):                      #checking whether the current index is even
-        even_index.insert(0, word[letter])      #adding the current element to even list
-    else:
-        odd_index.append(word[letter])          #in case of odd, adding the current element to odd list
+print("\nLetter in even index : ")
+if(len(word) % 2 == 0):             #checking whether the length of the word is even to fix the starting index
+    startValue = len(word) - 2      #initializing start value
+else:
+    startValue = len(word) - 1      #initializing start value
 
-print("Letters in odd index : ")
-print(*odd_index, sep = "")                     #printing elements in odd index
-print("Letters in even index : ")
-print(*even_index, sep = "")                    #printing elements in even index
+for letter in range(startValue, -1, -2):       #iterating through the word by even index
+    print(word[letter], end = "")               #printing the letter
 
 
 
@@ -21,20 +20,20 @@ test case 1
 Enter a word : abcd
 Letter in odd index : 
 bd
-Letter in even index :
+Letter in even index : 
 ca
 
 test case 2
 Enter a word : Apple
-Letters in odd index : 
+Letter in odd index : 
 pl
-Letters in even index :
+Letter in even index :
 epA
 
 test case 3
 Enter a word : Apple#Google
-Letters in odd index : 
+Letter in odd index : 
 pl#oge
-Letters in even index : 
+Letter in even index :
 loGepA
 """
