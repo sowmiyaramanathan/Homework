898dbfe33abfc7dde308dde9bc12bd210c3a02d8
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-09-02 21:28:46 +0530

    Changed_spelling

--- Week_8/Merge_strings.py
+++ Week_8/Merge_strings.py
@@ -2,7 +2,7 @@ firstWord = input("Enter first word : ")            #reading first word
 secondWord = input("Enter second word : ")          #reading second word
 
 if(len(firstWord) == len(secondWord)):              #checking whether the length of the given strings is same
-    print("Mergrd String:")
+    print("Merged String:")
     for letter in range(len(firstWord)):            #in case of same, iterating through the first input string
         print(firstWord[letter], end = "")
         print(secondWord[letter], end = "")
@@ -17,7 +17,7 @@ else:                                               #if not same, printing not s
 test case 1
 Enter first word : abcd
 Enter second word : 1234
-Mergrd String:
+Merged String:
 a1b2c3d4
 ---------------------------------
 
