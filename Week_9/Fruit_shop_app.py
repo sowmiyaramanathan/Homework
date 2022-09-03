b8222bd8fad8541aee562b998ec4d757375220d0
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-09-03 23:05:52 +0530

    Added_Problem_Statement

--- Week_9/Fruit_shop_app.py
+++ Week_9/Fruit_shop_app.py
@@ -1,3 +1,6 @@
+"""Write an app for the fruit vendor. Fruit vendor has a list of fruits (apple, Orange, banana etc). When the customer comes in, vendor asks "What do you want to buy?". The customer can say "I want apple", or "Apple" or "three apple" or something like that. Your program will find out what fruit the customer is asking. 
+Your program will also find, if the customer already asked for the quantity of the fruit (one apple or 5 orange etc). Print the quantity if the customer says the quantity. If not, ask him how much he wants. You can limit the quantity to single digit number."""
+
 fruitsList = ["apple", "orange", "banana", "melon", "cherry"]       #initializing fruits list
 quantity = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] #initializing quantity needed
 
