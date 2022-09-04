ccfce408f667df6d43b16d8e47f70a74ff647a5f
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-09-04 09:24:34 +0530

    test_file

--- Github_Exercise/test.py
+++ Github_Exercise/test.py
@@ -1,7 +1,3 @@
-word = input("Enter a word : ")
-reversed_word = ""
-
-for letter in range(len(word)-1, -1, -1):
-    reversed_word += word[letter]
-
-print(reversed_word)
+word = "dog"
+val = len(word) - 1
+print(word.replace(word[val], ""))
