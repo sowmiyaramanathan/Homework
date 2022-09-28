59d307f2b1a255f14127f8fd0bb90e3c76268baa
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-09-28 21:35:26 +0530

    File_handling_homework_finished

---
+++ Week_13/File_Handling.py
@@ -0,0 +1,11 @@
+file1 = open(r"C:\Users\user\Desktop\Sayur Learning\Sowmiya\Week_13\Input_File.txt", "r")   #opening input file in read mode
+file2 = open(r"C:\Users\user\Desktop\Sayur Learning\Sowmiya\Week_13\Output_File.txt", "w")  #opening output file in write mode to write contents into it
+for line in file1:                              #looping through each line
+    file2.write(line)                           #writing each line to the output file
+
+file1.close()                                   #closing input file
+file2.close()                                   #closing output file
+
+file2 = open(r"C:\Users\user\Desktop\Sayur Learning\Sowmiya\Week_13\Output_File.txt", "r")  #opening output file in read mode to read the contents
+print(file2.read())                              #printing the contents of the output file
+file2.close()                                    #closing the output file
