b450e78f827dbbdffb1a2821bbb3c193e74d340a
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-10-03 00:02:17 +0530

    Displayed_the_product

--- Week_14/Findind_max_price_in_CSV.py
+++ Week_14/Findind_max_price_in_CSV.py
@@ -1,5 +1,6 @@
-file = open(r"C:\Users\user\Desktop\Sayur Learning\Sowmiya\Week_14\input.csv", "r")                     #opening the input file in read mode
-priceIndex = 1           #setting the index value of price of the cafe items           
+file = open(r"C:\Users\user\Desktop\Sayur Learning\Sowmiya\Week_14\input.csv", "r")  #opening the input file in read mode
+priceIndex = 1                    #setting the index value of price of the cafe items  
+itemIndex = 0                     #setting the index value of the item         
 maximumPrice = 1                  #assigning maximumPrice value as 1
 
 next(file)                        #skipping the header row
@@ -8,7 +9,13 @@ for item in file:                 #iterating through the file one line at a time
     priceOfTheItem = int(temp[priceIndex])  #getting the price of the item
     if(maximumPrice <= priceOfTheItem):              #comparing it with the maximumPrice
         maximumPrice = priceOfTheItem                #if the maximumPrice is less, changing the maximumPrice to the current loop price
+        itemName = temp[itemIndex]
 
-print("Maximum price in the list : ", maximumPrice)      #printing the maximumPrice
+print("Maximum price in the list : ", maximumPrice, " and the product is : ", itemName)      #printing the maximumPrice
 
 file.close()                                    #closing the file
+
+
+"""
+Maximum price in the list :  50  and the product is :  Coke
+"""
