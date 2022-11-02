f5ebbbf91f4e702c1b4a487f2542e37c245a82c7
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-11-02 21:07:28 +0530

    Web_Programming_HomeWork_Finished

--- Week_21/webserver.py
+++ Week_21/webserver.py
@@ -10,4 +10,29 @@ def home():
 def add(numOne, numTwo):
     return f'Addition is {numOne + numTwo}'
 
+@app.route('/sub/<int:numOne>/<int:numTwo>')
+def sub(numOne, numTwo):
+    return f'Difference is {numOne - numTwo}'
+
+@app.route('/mul/<int:numOne>/<int:numTwo>')
+def mul(numOne, numTwo):
+    return f'Product is {numOne * numTwo}'
+
+@app.route('/div/<int:numOne>/<int:numTwo>')
+def div(numOne, numTwo):
+    return f'Division is {numOne / numTwo}'
+
+
+@app.route('/calc')
+def calc():
+    return render_template('calc.html')
+
+@app.route('/sqr/<int:numOne>')
+def sqr(numOne):
+    return f'Square of {numOne} is {numOne * numOne}'
+
+@app.route('/cub/<int:numOne>')
+def cub(numOne):
+    return f'Cube of {numOne} is {numOne * numOne * numOne}'
+
 app.run()
