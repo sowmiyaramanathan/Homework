699ba8422aee04506b045c9841c9d40b6a405ad4
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-11-08 01:00:58 +0530

    finished_inserting_data_using_loop

---
+++ Week_20/Adding_Data_Using_Loop.py
@@ -0,0 +1,32 @@
+from configparser import ConfigParser
+import mysql.connector as msconnector
+from faker import Faker
+from collections import defaultdict
+import pandas as pd
+
+config = ConfigParser()
+config.read("C:\\Users\\user\Desktop\Sayur Learning\Sowmiya\Week_20\\connection.ini")
+
+config_data  = config["credentials"]
+
+mydatabase = msconnector.connect(
+    host = config_data["host"],
+    user = config_data["username"],
+    password = config_data["password"],
+    database = config_data["dbname"]
+)
+
+mycursor = mydatabase.cursor()
+
+fake = Faker()
+row= {}
+n = 0
+cursor = mydatabase.cursor(buffered=True)
+
+while n >= 100:
+    n += 1
+    row = [fake.first_name(), fake.last_name(), fake.city(), fake.state(), fake.phone_number()]
+    mycursor.execute('INSERT INTO `tblpersons` (personFirstName, personLastName, personCity, personState, personPhNo) VALUES ("%s", "%s", "%s", "%s", "%s");' %(row[0], row[1], row[2], row[3], row[4]))
+    if n % 100 == 0:
+        print("iteration is ", n)
+        mydatabase.commit()
