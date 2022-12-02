fb85435e7878698899754d24f7e301281823884f
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2022-12-02 18:24:57 +0530

    homeworks

--- Week_19/CovidDataDisplay.py
+++ Week_19/CovidDataDisplay.py
@@ -1,5 +1,3 @@
-"""Inserting fake data into a data base using loop"""
-
 from configparser import ConfigParser       #importing configparser to configure credentials to establish connection to mysql data base
 import mysql.connector as msconnector       #importing mysql.connector to connect mysql database
 
@@ -17,11 +15,30 @@ mydatabase = msconnector.connect(
 
 mycursor = mydatabase.cursor()                  #initializing cursor to the database
 
-mycursor.execute('SELECT city FROM tbl_coviddata ORDER BY affected DESC')    #initializing mycursor to order the cities in descending order by affected case
-myResult = mycursor.fetchone()                              #getting the first data and storing it in a variable
-print("City with most affected case : ", *myResult)         #printing the most affected city
+# mycursor.execute('SELECT city FROM tbl_coviddata ORDER BY affected DESC')    #initializing mycursor to order the cities in descending order by affected case
+# myResult = mycursor.fetchone()                              #getting the first data and storing it in a variable
+# print("City with most affected case : ", *myResult)         #printing the most affected city
+
+# mycursor.reset()
+# mycursor.execute('SELECT city FROM tbl_coviddata ORDER BY recovered DESC')    #initializing mycursor to order the cities in descending order by recovered case
+# myResult = mycursor.fetchone()                                #getting the first data and storing it in a variable
+# print("City with high recovery status : ", *myResult)         #printing the most affected city
 
 
-mycursor.execute('SELECT city FROM tbl_coviddata ORDER BY recovered DESC')    #initializing mycursor to order the cities in descending order by recovered case
-myResult = mycursor.fetchone()                                #getting the first data and storing it in a variable
-print("City with high recovery status : ", *myResult)         #printing the most affected city
+# data = [("India", "TamilNadu", "KanyaKumari", 14000, 10000, "15-30", "20%"),
+#         ("India", "TamilNadu", "Karur", 12400, 9000, "20-60", "15%"),
+#         ("India", "TamilNadu", "Dindugal", 12000, 5400, "30-50", "21%"),
+#         ("India", "TamilNadu", "Theni", 17000, 7000, "45-74", "40%")]
+
+# insertStatement = 'INSERT INTO tbl_coviddata VALUES (%s, %s, %s, %s, %s, %s, %s)'
+
+# mycursor.executemany(insertStatement, data)
+# mydatabase.commit()
+
+# mycursor.execute("UPDATE tbl_coviddata set deathrate = replace(deathrate, "%", "")")
+mycursor.execute('select city from tbl_coviddata order by convert(deathrate, decimal) desc')
+result = mycursor.fetchmany(size = 3)
+for x in result:
+    print(x)
+
+# mydatabase.commit()
--- Week_22/server.py
+++ Week_22/server.py
@@ -1,4 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from configparser import ConfigParser       #importing configparser to configure credentials to establish connection to mysql data base
+import mysql.connector as msconnector       #importing mysql.connector to connect mysql database
+
+config = ConfigParser()                     #initializing configparser method
+config.read("C:\\Users\\user\Desktop\Sayur Learning\Sowmiya\Week_22\\connection.ini")   #reading the credentials file
+config_data  = config["credentials"]            #initializing the credentials data
+mydatabase = msconnector.connect(
+    host = config_data["host"],
+    user = config_data["username"],
+    password = config_data["password"],
+    database = config_data["dbname"]
+)                                               #establishing connection to database
+mycursor = mydatabase.cursor()                  #initializing cursor to the database
+cursor = mydatabase.cursor(buffered=True)       #making the buffer to true to store the data 
 
 app = Flask(__name__)
 
@@ -9,5 +23,35 @@ def choice():
 @app.route('/addproducts')
 def add():
     return render_template('add.html')
+
+@app.route('/addproduct', methods = ["POST"])
+def adding():
+    kolejPazarID = request.form.get("kolejPazarID")
+    prdtName = request.form.get("prdtName")
+    prdtBrand = request.form.get("prdtBrand")
+    prdtDescrp = request.form.get("prdtDescrp")
+    prdtMRP = request.form.get("prdtMRP")
+    prdtSP = request.form.get("prdtSP")
+    prdtMarg = request.form.get("prdtMarg")
+    prdtStk = request.form.get("prdtStk")
+    add = (kolejPazarID, prdtName, prdtBrand, prdtDescrp, int(prdtMRP), int(prdtSP), int(prdtMarg), int(prdtStk))
+    print(add)
+    insertStatement = 'INSERT INTO `tbl_products` (kolejPazarID, productName, productBrand, productDescription, productProfitMargin, productMRP, productSP, productInStock) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);'
+    mycursor.execute(insertStatement, add)
+    mydatabase.commit()
+    return ''
+ 
+@app.route('/viewproducts')
+def view():
+    return render_template('view.html')
+
+@app.route('/view')
+def viewPrdts():
+    data = []
+    mycursor.execute('SELECT * FROM tbl_products')
+    result = mycursor.fetchall()
+    for index in result:
+        data.append(index)
+    return data
 
 app.run()
