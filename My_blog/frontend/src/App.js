79241281cd55ac5895b3a21ee39bf00b0de93589
Sowmiya Ramanathan <ramanathan.sowmiya02@gmail.com>
2023-01-01 23:10:42 +0530

    pages

--- My_blog/frontend/src/App.js
+++ My_blog/frontend/src/App.js
@@ -1,68 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react'
-import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./Components/MainPage/MainPage";
+import SignupPage from './Components/SignupPage/SignupPage';
+import SigninPage from './Components/SigninPage/SigninPage';
 
 function App() {
-
-  const [ data, setData ] = useState([{}])
-
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: "/data"
-    })
-    .then((response) => {
-      const res = response.data
-      setData(({
-        data: res.Name
-      }))
-      console.log(data.data)
-    }).catch((error) => {
-      if(error.response) {
-        console.log(error.response)
-      }
-    })
-  }, [])
-
-  const [ name, setName ] = useState([{}])
-
-  function onClick () {
-    axios({
-      method: "GET",
-      url: '/data'
-    })
-    .then((response) => {
-      const res = response.data
-      setName(({
-        name: res.Name
-      }))
-      console.log(name)
-    }).catch((error) => {
-      if(error.response) {
-        console.log(error.response)
-      }
-    })
-  }
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={onClick}>Click here</button>
-        <p>{name.name}</p>
-      </header>
+      <Routes>
+          <Route path="/" element={<MainPage />}/>
+          <Route path="/signup" element={<SignupPage />}/>
+          <Route path="/signin" element={<SigninPage />}/>
+        </Routes>
     </div>
   );
 }
